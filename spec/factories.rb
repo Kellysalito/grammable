cc495603740c0941a5ba33cc69e09877ba9b43b3
Kresmlin <kycmmas@gmail.com>
2019-03-30 03:47:31 +0000

    add a detail page for grams first part

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,5 +7,10 @@ factory :user do
   password { "secretPassword" }
   password_confirmation { "secretPassword" }
   end
+
+  factory :gram do
+    message { "hello" }
+    association :user
+  end
   
 end
